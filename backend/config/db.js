4536fc22a4070b78cf052d22ba8b162fca48907a
santi <120440622+Santi-rivera-prada@users.noreply.github.com>
2023-11-14 23:29:55 -0500

    finish

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,16 @@
-//modificacion erronea
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
